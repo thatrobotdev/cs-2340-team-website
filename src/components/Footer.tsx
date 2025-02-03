8c0c2b82308999eb053ec3c8355ccf3a6cd1b6b7
James Kerrane <hi@jameskerrane.com>
2025-02-03 01:24:17 -0500

    Fix links in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: React.FC = () => {
     <footer>
       <hr className="mt-5 mb-4" />
       <p className="text-muted">
-        Built with <a href="https://getbootstrap.com/">Bootstrap 5</a>, <a href="">React</a>, <a href="https://react-bootstrap.github.io/">React Bootstrap</a>, <a href="https://nextjs.org/">Next.js</a>, and <HeartFill title="heart icon" aria-label="love" color="red"/> in Atlanta, GA.
+        Built with <a href="https://getbootstrap.com/">Bootstrap 5</a>, <a href="https://react.dev">React</a>, <a href="https://react-bootstrap.github.io/">React Bootstrap</a>, <a href="https://nextjs.org/">Next.js</a>, and <HeartFill title="heart icon" aria-label="love" color="red"/> in Atlanta, GA.
         Created and open sourced by the CS 2340 project team. Licensed MIT.
       </p>
     </footer>
