b56d8011da16a972030163e21753d9ed2f8c1c22
James Kerrane <hi@jameskerrane.com>
2025-02-02 21:58:58 -0500

    Configure GitHub Pages hosting
    - Update caniuse-lite
    - Add action, publish GitHub workflows following tutorial at https://www.freecodecamp.org/news/how-to-deploy-next-js-app-to-github-pages/

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Allow for static publishing on GitHub Pages at subdomain https://thatrobotdev.github.io/cs-2340-team-website/
+  basePath: "/cs-2340-team-website",
+  /*
+    Enable static export to allow for HTML/CSS/JS static assets hosting on GitHub Pages
+    
+    See https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+  */ 
+  output: "export",
   reactStrictMode: true,
 }
 
