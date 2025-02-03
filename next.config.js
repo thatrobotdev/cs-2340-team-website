302ace4b7b05ed84ff43a66e52d102d14ca3674c
James Kerrane <hi@jameskerrane.com>
2025-02-02 22:23:57 -0500

    Fix GitHub Actions workflow
    - Use https://github.com/gregrickaby/nextjs-github-pages for deploying to GitHub Pages
    - Place .nojekyll file in /public directory to disable GitHub Pages from creating a Jekyll website
    - Use JSDoc comments in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,12 +2,20 @@
 const nextConfig = {
   // Allow for static publishing on GitHub Pages at subdomain https://thatrobotdev.github.io/cs-2340-team-website/
   basePath: "/cs-2340-team-website",
-  /*
-    Enable static export to allow for HTML/CSS/JS static assets hosting on GitHub Pages
-    
-    See https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
-  */ 
+  /**
+   * Enable static export to allow for HTML/CSS/JS static assets hosting on GitHub Pages
+   * @see https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+   */
   output: "export",
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
 }
 
