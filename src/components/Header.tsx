d868229029a8c23313de4d8340d0bc69b8896922
Palash Patel <palashpatel@lawn-143-215-98-47.lawn.gatech.edu>
2025-02-07 00:25:17 -0500

    LOT OF STYLING CHANGES

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,8 +7,8 @@ const Header: React.FC = () => {
       <Container>
         {/* Brand / Logo */}
         <Navbar.Brand>
-          <Link href="/" className="text-light fw-bold text-decoration-none">
-            CS 2340 Team 4
+          <Link href="/" className="text-light fw-bold text-decoration-none hover-style" style={{color: "#"}}>
+            CS 2340 TEAM 4
           </Link>
         </Navbar.Brand>
 
@@ -54,15 +54,27 @@ const navbarStyle: React.CSSProperties = {
   padding: "1rem",
   boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
   zIndex: 10,
+  top: 0,
+
+  /*
+  backgroundColor: "rgba(34, 34, 34, 0.9)",
+  backdropFilter: "blur(10px)",
+  padding: "1rem",
+  boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+  zIndex: 10,
+  position: "fixed", // Keeps the header fixed at the top
+  top: 0, // Aligns it to the top of the viewport
+  width: "100%", // Ensures it spans the full width of the page
+  */
 };
 
 const githubLinkStyle: React.CSSProperties = {
   fontWeight: "bold",
-  border: "2px solid #6A8EAE",
+  border: "2px solid #FF5252",
   padding: "6px 12px",
   borderRadius: "5px",
   transition: "all 0.3s ease-in-out",
-  color: "#6A8EAE",
+  color: "#FF5252",
   textDecoration: "none",
   cursor: "pointer",
 };
--- src/pages/projects/moviesstore.tsx
+++ src/pages/projects/moviesstore.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Container from "react-bootstrap/Container";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import BackImg from "@/components/BackImg";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Container as="main" className="py-4 px-3 mx-auto">
+        <BackImg />
         <Header />
 
         <h1>moviesstore</h1>
